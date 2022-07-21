71742cc24c634015f9358e3f3c264821086ec3a3
YurkiewB <Bryurkiewicz09@icloud.com>
2022-07-21 12:10:26 -0700

    fixing all issues

--- script.js
+++ script.js
@@ -1,46 +1,70 @@
- console.log("Hello lets play!")//Hello.
-//overall, rps runs the complete game.
+console.log("Hello lets play!")//Hello.
+let user;
+let cpu;
+let computerScore = 0;
+let playerScore = 0;
+const buttons = document.querySelectorAll("button");
+const container = document.querySelector("#results");
 
-function rockPaperScissors(){ 
-    //makes sure the user inputs either rock,paper,or scissors. 
-        function myPlay(){ 
-                if (user === "rock"||button==="paper"||user==="scissors") {
-                console.log("user played "+ user )
-    }           else {
-       console.log( "You didn't make the right selection" );
-        }
-    }
-    myPlay()
-
-        const moves=["rock","paper","scissors"]//defines cpu moveset
-        let cpu=moves[Math.floor(Math.random()* moves.length)]//radomizes cpu moveset
-        function computerPlay(){//reports to the console what comp plays
-                console.log("computer played "+ cpu)
-    }
-        computerPlay()
-
-        function game(){//game conditionals comparing user to cpu. determines winner
-        if (user === cpu){
-            console.log("You and the computer played the same move, go again.");
-    }   else if (user==="rock" && cpu==="scissors" ){
-            console.log("You played Rock, Computer played Scissors: You win! Good job, test your luck agian? refresh.");
-    }   else if (user==="scissors" && cpu==="rock"){
-            console.log("You played Scissors, Computer played rock: You loose! Try again. Refresh.");
-    }   else if (user==="paper" && cpu==="rock"){
-            console.log("You played Paper, Computer played Rock: You win! Good job, test your luck agian? refresh.");
-    }   else if (user==="rock" && cpu==="paper"){
-            console.log("You played Rock, Computer played Paper: You loose! Try again. Refresh.");
-    }   else if (user==="scissors" && cpu==="paper"){
-            console.log("You played Scissors, Computer played Paper: You win! Good job, test your luck agian? refresh.")
-    }   else if (user==="paper" && cpu==="scissors"){
-            console.log("You played Paper, Computer played Scissors: You loose! Try again. Refresh.")
-        }
-    }
-    game()
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const img = button.querySelector("img");
+    user = img.alt.toLowerCase();
+    RockPaperScissors(user, cpu);
+  });
+});
 
 
+const compMoves = ["rock", "paper", "scissors"];
+
+function computerPlay() {
+  return compMoves[Math.floor(Math.random()*compMoves.length)];
 }
-//rockPaperScissors()
-//retiterates the games multple times!
-//for (let i = 0; i < 5; i++){
-//    rockPaperScissors()
+
+function RockPaperScissors(user, cpu) {
+  cpu = computerPlay().toLowerCase();
+  user = user.toLowerCase();
+  console.log(`You played ${user}`);
+  console.log(`Computer played ${cpu}`);
+  if (user === cpu){
+    updateResults("You and the computer played the same move, go again.");
+}   else if (user==="rock" && cpu==="scissors" ){
+    updateResults("You played Rock, Computer played Scissors: You win! Good job, test your luck agian?");
+    playerScore = ++playerScore;
+    keepPlayerScore();
+}   else if (user==="scissors" && cpu==="rock"){
+    updateResults("You played Scissors, Computer played rock: You loose! Try again.");
+    computerScore = ++computerScore;
+    keepCompScore();
+}   else if (user==="paper" && cpu==="rock"){
+    updateResults("You played Paper, Computer played Rock: You win! Good job, test your luck agian? ");
+    playerScore = ++playerScore;
+    keepPlayerScore();
+}   else if (user==="rock" && cpu==="paper"){
+    updateResults("You played Rock, Computer played Paper: You loose! Try again.");
+    computerScore = ++computerScore;
+    keepCompScore();
+}   else if (user==="scissors" && cpu==="paper"){
+    updateResults("You played Scissors, Computer played Paper: You win! Good job, test your luck agian?");
+    playerScore = ++playerScore;
+    keepPlayerScore();
+}   else if (user==="paper" && cpu==="scissors"){
+    updateResults("You played Paper, Computer played Scissors: You loose! Try again.");
+    computerScore = ++computerScore;
+    keepCompScore();
+}
+};
+
+function updateResults(str){
+  container.textContent = str;
+}
+
+function keepPlayerScore(str){
+  let playerScoreBox = document.querySelector("#playScore");
+  playerScoreBox.textContent = playerScore;
+}
+
+function keepCompScore(str){
+  let computerScoreBox = document.querySelector("#compScore");
+  computerScoreBox.textContent = computerScore;
+}
